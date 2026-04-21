0f34b0eb0d28a14727a85fc0df02c419f33a6603
linhbui58 <23070525@vnu.edu.vn>
2026-04-21 08:49:27 +0700

    week11

--- mvc mini project/project/app/models/Product.php
+++ mvc mini project/project/app/models/Product.php
@@ -1,1 +1,13 @@
+<?php
 
+class Product {
+    public static $products = [];
+
+    public static function all() {
+        return self::$products;
+    }
+
+    public static function create($name) {
+        self::$products[] = $name;
+    }
+}
