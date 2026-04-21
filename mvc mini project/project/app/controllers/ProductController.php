c77a37892e697a8219fcf464e6733258e66225ad
linhbui58 <23070525@vnu.edu.vn>
2026-04-21 08:47:56 +0700

    week11

--- mvc mini project/project/app/controllers/ProductController.php
+++ mvc mini project/project/app/controllers/ProductController.php
@@ -1,1 +1,29 @@
+<?php
 
+require_once '../app/models/Product.php';
+
+class ProductController {
+
+    public function index() {
+        $products = Product::all();
+        require '../app/views/products/index.php';
+    }
+
+    public function create() {
+        require '../app/views/products/create.php';
+    }
+
+    public function store() {
+
+        $name = $_POST['name'] ?? '';
+
+        if ($name != '') {
+            Product::create($name);
+        }
+
+        // redirect chuẩn theo project
+        $base = dirname($_SERVER['SCRIPT_NAME']);
+        header("Location: $base/index.php/products");
+        exit;
+    }
+}
